7d179e35b46d518685c8c8bdfbca98ee828b1a41
Agostinhoneto <agostneto6@gmail.com>
2023-12-06 17:29:14 -0300

    criando auth

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,8 @@
 <?php
 
 use App\Http\Controllers\DespesasController;
+use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\ReceitasController;
-use App\Models\Despesas;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -20,19 +20,23 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
+
+
 Route::get('/receitas', [ReceitasController::class, 'index'])->name('listar_receitas');
 Route::get('/receitas/create', [ReceitasController::class, 'create']);
 Route::post('/receitas/store', [ReceitasController::class, 'store'])->name('criar_receitas');
-
-
 Route::get('/despesas', [DespesasController::class, 'index'])->name('listar_despesas');
 Route::get('/despesas/create', [DespesasController::class, 'create']);
 Route::post('/despesas/store', [DespesasController::class, 'store'])->name('criar_despesas');
 
-/*
-Route::delete('/series/{id}', 'SeriesController@destroy');
-Route::post('/series/{id}/editaNome', 'SeriesController@editaNome');
-Route::get('/series/{serieId}/temporadas', 'TemporadasController@index');
-Route::get('/temporadas/{temporada}/episodios', 'EpisodiosController@index');
-Route::post('/temporadas/{temporada}/episodios/assistir', 'EpisodiosController@assistir');
-*/
