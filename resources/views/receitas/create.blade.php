3afba2f9aa51524d7d803d84541581698219f480
Agostinhoneto <agostneto6@gmail.com>
2023-12-10 11:29:59 -0300

    refatorando

--- resources/views/receitas/create.blade.php
+++ resources/views/receitas/create.blade.php
@@ -1,40 +1,47 @@
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <meta name="referrer" content="always">
+    <title>Admin</title>
+    @vite('resources/css/app.css')
+    @vite('resources/js/app.js')
+</head>
 @extends('layout')
-
-@section('cabecalho')
-Adicionar Receitas
-@endsection
-
-@section('conteudo')
-@if ($errors->any())
-<div class="alert alert-danger">
-    <ul>
-        @foreach ($errors->all() as $error)
-        <li>{{ $error }}</li>
-        @endforeach
-    </ul>
-</div>
-@endif
-
-<form action="{{ route('receitas.store')}}" method="post">
-    @csrf
+<div x-data="{ sidebarOpen: false }" class="flex h-screen bg-gray-200 font-roboto">
+    @include('layouts.sidebar')
     <div class="container" style="margin-top:40px;">
-        <div class="row">
-            <div class="col col-6">
-                <label for="descricao">Descrição da Receita</label>
-                <input type="text" id="descricao" class="form-control" name="descricao" required>
-            </div>
-
-            <div class="col col-2">
-                <label for="valor">Valor</label>
-                <input type="number" id="valor" class="form-control" name="valor" required>
-            </div>
-
-            <div class="col col-2">
-                <label for="data">Data</label>
-                <input type="date" id="data_recebimento" class="form-control" name="data_recebimento" required>
-            </div>
+        @if ($errors->any())
+        <div class="alert alert-danger">
+            <ul>
+                @foreach ($errors->all() as $error)
+                <li>{{ $error }}</li>
+                @endforeach
+            </ul>
         </div>
-        <button class="btn btn-primary mt-2">Adicionar</button>
+        @endif
+
+        <form action="{{ route('receitas.store')}}" method="post">
+            @csrf
+            <div class="container" style="margin-top:40px;">
+                <div class="row">
+                    <div class="col col-6">
+                        <label for="descricao">Descrição da Receita</label>
+                        <input type="text" id="descricao" class="form-control" name="descricao" required>
+                    </div>
+
+                    <div class="col col-2">
+                        <label for="valor">Valor</label>
+                        <input type="number" id="valor" class="form-control" name="valor" required>
+                    </div>
+
+                    <div class="col col-2">
+                        <label for="data">Data</label>
+                        <input type="date" id="data_recebimento" class="form-control" name="data_recebimento" required>
+                    </div>
+                </div>
+                <button class="btn btn-primary mt-2">Adicionar</button>
+            </div>
+        </form>
     </div>
-</form>
-@endsection
+</div>
