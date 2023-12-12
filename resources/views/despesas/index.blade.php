d51ae9752cef4b4bac191134c7a2b7056e1b7f94
Agostinhoneto <agostneto6@gmail.com>
2023-12-12 10:28:28 -0300

    criando status

--- resources/views/despesas/index.blade.php
+++ resources/views/despesas/index.blade.php
@@ -18,6 +18,71 @@
                     <div class="card-header">
                         <h3 class="card-title">Despesas</h3>
                     </div>
+
+
+                    <!-- Botão que abre o modal -->
+                    <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#myModal">
+                        Abrir Modal
+                    </button>
+
+                    <!-- O Modal -->
+                    <div class="modal fade" id="myModal">
+                        <div class="modal-dialog">
+                            <div class="modal-content">
+                                <!-- Cabeçalho do Modal -->
+                                <div class="modal-header">
+                                    <h4 class="modal-title">Criar Despesas</h4>
+                                    <button type="button" class="close" data-dismiss="modal">&times;</button>
+                                </div>
+                                <!-- Corpo do Modal -->
+                                <div class="modal-body">
+                                    <div class="row">
+                                        <!-- left column -->
+                                        <div class="col-md-12">
+                                            <!-- general form elements -->
+                                            <div class="card card-primary">
+                                                <br>
+                                                <form class="form" method="POST" action="{{ route('despesas.store') }}">
+                                                    @csrf
+                                                    <div class="col col-8">
+                                                        <label for="descricao">Descrição:</label>
+                                                        <input type="text" class="form-control" name="descricao" id="descricao" required placeholder="Descrição">
+                                                    </div>
+                                                    <br>
+                                                    <div class="col col-4">
+                                                        <label for="valor">Valor</label>
+                                                        <input type="number" class="form-control" name="valor" id="valor" required placeholder="Valor $$">
+                                                    </div>
+                                                    <br>
+                                                    <div class="col col-4">
+                                                        <label for="data_pagamento">Data do Pagamento</label>
+                                                        <input type="date" class="form-control" name="data_pagamento" id="data_pagamento" required placeholder="Data">
+                                                    </div>
+                                                    <br>
+                                                    <div class="col col-2">
+                                                        <div class="form-check">
+                                                            <input type="checkbox" name="status" class="form-check-input" id="exampleCheck1">
+                                                            <label class="form-check-label" for="exampleCheck1">Status</label>
+                                                        </div>
+                                                    </div>
+                                                    <br>
+                                                    <div class="col col-2">
+                                                        <button class="btn btn-primary mt-2">Salvar</button>
+                                                    </div>
+                                                </form>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <!-- Rodapé do Modal -->
+                                <div class="modal-footer">
+                                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Fechar</button>
+                                    <!-- Adicione outros botões conforme necessário -->
+                                </div>
+
+                            </div>
+                        </div>
+                    </div>
                     <!-- /.card-header -->
                     <div class="card-body">
                         <table id="example1" class="table table-bordered table-striped">
@@ -27,7 +92,10 @@
                                     <th scope="col">Descrição da Despesa</th>
                                     <th scope="col">Data do Pagamento</th>
                                     <th scope="col">Valor</th>
-                                    <th scope="col">Ações</th>
+                                    <th scope="col">Status</th>
+                                    <th scope="col"></th>
+                                    <th scope="col"></th>
+                                    <th scope="col"></th>
                                 </tr>
                             </thead>
                             <tbody>
@@ -45,22 +113,27 @@
                                             <a href="/despesa/{{ $despesa->id }}/temporadas" class="btn btn-info btn-sm mr-1">
                                                 <i class="fas fa-external-link-alt"></i>
                                             </a>
-                                            <form method="post" action="/despesa/{{ $despesa->id }}" onsubmit="return confirm('Tem certeza que deseja remover {{ addslashes($despesa->descricao) }}?')">
-                                                @csrf
-                                                @method('DELETE')
-                                                <button class="btn btn-danger btn-sm">
-                                                    <i class="far fa-trash-alt"></i>
-                                                </button>
-                                            </form>
+                                    </td>
+                                    <td>
+                                        <form method="post" action="/despesa/{{ $despesa->id }}" onsubmit="return confirm('Tem certeza que deseja remover {{ addslashes($despesa->descricao) }}?')">
+                                            @csrf
+                                            @method('DELETE')
+                                            <button class="btn btn-danger btn-sm">
+                                                <i class="far fa-trash-alt"></i>
+                                            </button>
+                                        </form>
                                         </span>
+                                    </td>
+                                    <td>
+                                        <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#myModal">
+                                            Abrir Modal
+                                        </button>
                                     </td>
                                 </tr>
                                 @endforeach
                             </tbody>
                         </table>
-                        <a href="{{ route('despesas.create') }}"></a>  
-                        <i class="fa-solid fa-file-lines">'</i>
-                        <i class="fa-solid fa-file-circle-plus">Adicionar</i>
+
         </ul>
     </div>
 </div>
