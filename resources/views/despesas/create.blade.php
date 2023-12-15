6acced838e0b908623c1addb2b0514c17bba5a03
Agostinhoneto <agostneto6@gmail.com>
2023-12-15 10:34:09 -0300

    criando pago e não pago

--- resources/views/despesas/create.blade.php
+++ resources/views/despesas/create.blade.php
@@ -1,17 +1,3 @@
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta http-equiv="x-ua-compatible" content="ie=edge">
-    <meta name="referrer" content="always">
-    <title>Admin</title>
-    @vite('resources/css/app.css')
-    @vite('resources/js/app.js')
-</head>
-@extends('layout')
-<div class="container-fluid">
-    <div x-data="{ sidebarOpen: false }" class="flex h-screen bg-gray-200 font-roboto">
-        @include('layouts.sidebar')
-        <div class="container" style="margin-top:40px;">
             @if ($errors->any())
             <div class="alert alert-danger">
                 <ul>
@@ -20,54 +6,72 @@
                     @endforeach
                 </ul>
             </div>
-            @endif
-            <!-- Main content -->
-            <section class="content">
-                <div class="container" style="margin-top:0px;">
-                    <div class="row">
-                        <!-- left column -->
-                        <div class="col-md-12">
-                            <!-- general form elements -->
-                            <div class="card card-primary">
-                                <div class="card-header">
-                                    <h3 class="card-title">Criar uma Receita</h3>
+            @endif<div class="modal fade" id="myModal">
+                <div class="modal-dialog">
+                    <div class="modal-content">
+                        <!-- Cabeçalho do Modal -->
+                        <div class="modal-header">
+                            <h4 class="modal-title">Criar Despesas</h4>
+                            <button type="button" class="close" data-dismiss="modal">&times;</button>
+                        </div>
+                        <!-- Corpo do Modal -->
+                        <div class="modal-body">
+                            <div class="row">
+                                <!-- left column -->
+                                <div class="col-md-12">
+                                    <!-- general form elements -->
+                                    <div class="card card-primary">
+                                        <br>
+                                        <form class="form" method="POST" action="{{ route('despesas.store') }}">
+                                            @csrf
+                                            <div class="col col-8">
+                                                <label for="descricao">Descrição:</label>
+                                                <input type="text" class="form-control" name="descricao" id="descricao" required placeholder="Descrição">
+                                            </div>
+                                            <br>
+                                            <div class="col col-6">
+                                                <label for="valor">Valor:</label>
+                                                <input type="number" class="form-control" name="valor" id="valor" required placeholder="Valor $$">
+                                            </div>
+                                            <br>
+                                            <div class="col col-6">
+                                                <label for="data_pagamento">Data do Pagamento:</label>
+                                                <input type="date" class="form-control" name="data_pagamento" id="data_pagamento" required placeholder="Data">
+                                            </div>
+                                            <br>
+                                            <div class="col col-6">
+                                                <label for="categoria_id">Categoria:</label>
+                                                <select name="categoria_id" id="categoria_id" class="form-control">
+                                                    <option>Selecione...</option>
+                                                    @foreach($categorias as $c)
+                                                    <option value="{{ $c->id }}">{{ $c->descricao }}</option>
+                                                    @endforeach
+                                                </select>
+                                            </div>
+                                            <br>
+                                            <div class="col col-6">
+                                                <label for="status">Status:</label>
+                                                <select id="status" name="status" class="form-control">
+                                                    <option>Selecione...</option>
+                                                    <option value="1">Ativo</option>
+                                                    <option value="0">Inativo</option>
+                                                </select>
+                                            </div>
+                                            <br>
+                                            <div class="col col-2">
+                                                <button class="btn btn-primary mt-2">Salvar</button>
+                                            </div>
+                                        </form>
+                                    </div>
                                 </div>
-                                <br>
-                                <!-- /.card-header -->
-                                <!-- form start -->
-                                <form class="form" method="POST" action="{{ route('despesas.store') }}">
-                                    @csrf
-                                    <div class="col col-8">
-                                        <label for="descricao">Descrição:</label>
-                                        <input type="text" class="form-control" name="descricao" id="descricao" required placeholder="Descrição">
-                                    </div>
-                                    <br>
-                                    <div class="col col-2">
-                                        <label for="valor">Valor</label>
-                                        <input type="number" class="form-control" name="valor" id="valor" required placeholder="Valor $$">
-                                    </div>
-                                    <br>
-                                    <div class="col col-2">
-                                        <label for="data_pagamento">Data do Pagamento</label>
-                                        <input type="date" class="form-control" name="data_pagamento" id="data_pagamento" required placeholder="Data">
-                                    </div>
-                                    <br>
-                                    <div class="col col-2">
-                                        <div class="form-check">
-                                            <input type="checkbox" name="status" class="form-check-input" id="exampleCheck1">
-                                            <label class="form-check-label" for="exampleCheck1">Status</label>
-                                        </div>
-                                    </div>
-                                    <br>
-                                    <div class="col col-2">
-                                        <button class="btn btn-primary mt-2">Adicionar</button>
-                                    </div>
-                                </form>
                             </div>
                         </div>
+                        <!-- Rodapé do Modal -->
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-secondary" data-dismiss="modal">Fechar</button>
+                        </div>
+
                     </div>
                 </div>
-            </section>
-        </div>
-    </div>
-</div>
+            </div>
+            <!-- /.card-header -->
--- resources/views/despesas/index.blade.php
+++ resources/views/despesas/index.blade.php
@@ -33,75 +33,7 @@
                     <div class="card-header">
                         <h3 class="card-title"></h3>
                     </div>
-                    <div class="modal fade" id="myModal">
-                        <div class="modal-dialog">
-                            <div class="modal-content">
-                                <!-- Cabeçalho do Modal -->
-                                <div class="modal-header">
-                                    <h4 class="modal-title">Criar Despesas</h4>
-                                    <button type="button" class="close" data-dismiss="modal">&times;</button>
-                                </div>
-                                <!-- Corpo do Modal -->
-                                <div class="modal-body">
-                                    <div class="row">
-                                        <!-- left column -->
-                                        <div class="col-md-12">
-                                            <!-- general form elements -->
-                                            <div class="card card-primary">
-                                                <br>
-                                                <form class="form" method="POST" action="{{ route('despesas.store') }}">
-                                                    @csrf
-                                                    <div class="col col-8">
-                                                        <label for="descricao">Descrição:</label>
-                                                        <input type="text" class="form-control" name="descricao" id="descricao" required placeholder="Descrição">
-                                                    </div>
-                                                    <br>
-                                                    <div class="col col-6">
-                                                        <label for="valor">Valor:</label>
-                                                        <input type="number" class="form-control" name="valor" id="valor" required placeholder="Valor $$">
-                                                    </div>
-                                                    <br>
-                                                    <div class="col col-6">
-                                                        <label for="data_pagamento">Data do Pagamento:</label>
-                                                        <input type="date" class="form-control" name="data_pagamento" id="data_pagamento" required placeholder="Data">
-                                                    </div>
-                                                    <br>
-                                                    <div class="col col-6">
-                                                        <label for="categoria_id">Categoria:</label>
-                                                        <select name="categoria_id" id="categoria_id" class="form-control">
-                                                            <option>Selecione...</option>
-                                                            @foreach($categorias as $c)
-                                                            <option value="{{ $c->id }}">{{ $c->descricao }}</option>
-                                                            @endforeach
-                                                        </select>
-                                                    </div>
-                                                    <br>
-                                                    <div class="col col-6">
-                                                        <label for="status">Status:</label>
-                                                        <select id="status" name="status" class="form-control">
-                                                            <option>Selecione...</option>
-                                                            <option value="1">Ativo</option>
-                                                            <option value="0">Inativo</option>
-                                                        </select>
-                                                    </div>
-                                                    <br>
-                                                    <div class="col col-2">
-                                                        <button class="btn btn-primary mt-2">Salvar</button>
-                                                    </div>
-                                                </form>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                                <!-- Rodapé do Modal -->
-                                <div class="modal-footer">
-                                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Fechar</button>
-                                </div>
-
-                            </div>
-                        </div>
-                    </div>
-                    <!-- /.card-header -->
+                    @include('despesas.create')
                     <div class="card-body">
                         <table id="example1" class="table table-bordered table-striped">
                             <thead>
