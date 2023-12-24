1e81decb17f029d9ed02ceda6d7c02bcfc3a09ac
Agostinhoneto <agostneto6@gmail.com>
2023-12-24 14:39:42 -0300

    criando relatorios

---
+++ resources/views/relatorios/index.blade.php
@@ -0,0 +1,15 @@
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <meta name="referrer" content="always">
+    <title>Admin</title>
+    @vite('resources/css/app.css')
+    @vite('resources/js/app.js')
+</head>
+@extends('layout')
+<div x-data="{ sidebarOpen: false }" class="flex h-screen bg-gray-200 font-roboto">
+    @include('layouts.sidebar')
+
+</div>
+
