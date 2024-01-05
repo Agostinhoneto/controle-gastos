843a1cd47976e689f26067008807d566e5a0ce63
Agostinhoneto <agostneto6@gmail.com>
2024-01-05 15:02:49 -0300

    criando e gerando pdf

--- app/Http/Controllers/RelatorioController.php
+++ app/Http/Controllers/RelatorioController.php
@@ -18,31 +18,21 @@ class RelatorioController extends Controller
 
     public function gerarPDF(Request $request)
     {
+        // Lógica para gerar o PDF com base nos filtros do formulário
+        $filter1 = $request->input('filter1');
+        $filter2 = $request->input('filter2');
+        // Consulta ao banco de dados com base nos filtros
+        $reports = Despesas::where('descricao', $filter1)
+            ->where('id', $filter2)
+            ->get();
+        $data = [
+            'descricao' => $filter1,
+            'id' => $filter2,
+        ];
+        $pdf = FacadePdf::loadView('relatorios.pdf', compact('data'));
 
-          // Lógica para gerar o PDF com base nos filtros do formulário
-          $filter1 = $request->input('filter1');
-          $filter2 = $request->input('filter2');
-          // Consulta ao banco de dados com base nos filtros
-          $reports = Despesas::where('descricao', $filter1)
-                          ->where('id', $filter2)
-                          ->get();
-          $data = [
-              'descricao' => $filter1,
-              'id' => $filter2,
-          ];
-  
-          $pdf = FacadePdf::loadView('relatorios.pdf', compact('data'));
+        return $pdf->download('relatorio.pdf');
 
-          return $pdf->download('relatorio.pdf');
-
-      //  dd($data); 
-       //   return $reports->download('relatorio.pdf');
-
-        /*
-        $despesas = Despesas::all();
-        $pdf = PDF::loadView('pdf', compact('despesas'));
-        return $pdf->setPaper('a4')->stream('relatorios.despesas.pdf');
-
-        */
+      
     }
 }
--- resources/views/relatorios/index.blade.php
+++ resources/views/relatorios/index.blade.php
@@ -15,28 +15,36 @@
             <div class="table-responsive">
                 <div class="card">
                     <div class="card-header">
-                        <h3 class="card-title">Relatórios</h3>
+                        <h1 class="card-title">Relatório PDF</h1>
                     </div>
-
-                    <h1>Relatório PDF</h1>
-                    <div class="container">
+                    <div class="row">
+                    <!-- left column -->
+                    <div class="col-md-12">
+                        <!-- general form elements -->
+                        <div class="card card-primary">              
                         <form action="{{ route('relatorios.despesas') }}" method="post">
                             @csrf
-                            <!-- Adicione seus campos de filtro aqui -->
-                            <label for="filter1">Descrição:</label>
-                            <input type="text" name="filter1">
-
-                            <label for="filter2">Categorias :</label>
-                            <input type="text" name="filter2">
-
-                            <label for="filter2">Data :</label>
-                            <input type="date" name="filter2">
-
-                            <button type="submit">Gerar PDF</button>
+                            <div class="form-group">
+                                <label for="filter1">Descrição:</label>
+                                <input type="text" name="filter1">
+                            </div>
+                            <div class="form-group">
+                                <label for="filter2">Categorias :</label>
+                                <input type="text" name="filter2">
+                            </div>
+                            <div class="form-group">
+                                <label for="filter2">Data :</label>
+                                <input type="date" name="filter2">
+                            </div>
+                            <br>
+                            <button type="submit" class="btn btn-primary">Gerar PDF</button>
                         </form>
                     </div>
                 </div>
             </div>
+                </div>
+            </div>
+        
         </ul>
     </div>
 </div>
