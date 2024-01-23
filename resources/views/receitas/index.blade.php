9b61e7156afd6c2f864464c56690c34071a76cea
Agostinhoneto <agostneto6@gmail.com>
2024-01-23 18:34:46 -0300

    alterando relatorios

--- resources/views/receitas/index.blade.php
+++ resources/views/receitas/index.blade.php
@@ -2,97 +2,98 @@
 @extends('layout')
 @include('mensagem', ['mensagem' => $mensagem])
 <style>
-    #status {
-        padding: 5px;
-    }
+#status {
+padding: 5px;
+}
 
-    .ativo {
-        background-color: green;
-        color: white;
-    }
+.ativo {
+background-color: green;
+color: white;
+}
 
-    .inativo {
-        background-color: red;
-        color: white;
-    }
+.inativo {
+background-color: red;
+color: white;
+}
 </style>
 <div x-data="{ sidebarOpen: false }" class="flex h-screen bg-gray-200 font-roboto">
-    @include('layouts.sidebar')
-    <div class="card-body">
-        <ul class="list-group">
-            <div class="table-responsive">
-                <div class="card">
-                    <div class="card-header">
-                        <h3 class="card-title"></h3>
+@include('layouts.sidebar')
+<div class="card-body">
+<ul class="list-group">
+    <div class="table-responsive">
+        <div class="card">
+            <div class="card-header">
+                <h3 class="card-title"></h3>
+            </div>
+            @include('receitas.create')
+            @include('components.flash-message')
+            <div class="card-body">
+                <div class="card-body">
+                    <table id="example1" class="table table-bordered table-striped">
+                        <thead>
+                            <tr>
+                                <th scope="col">#</th>
+                                <th scope="col">Descrição da Receita</th>
+                                <th scope="col">Data do Recebimento</th>
+                                <th scope="col">Valor</th>
+                                <th scope="col">Status</th>
+                                <th scope="col">Categoria</th>
+                                <th scope="col">Editar</th>
+                                <th scope="col">Excluir</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach($receitas as $receita)
+                            <tr>
+                                <th scope="row">{{ $receita->id }}</th>
+                                <td>{{ $receita->descricao }}</td>
+                                <td>{{Carbon\Carbon::parse( $receita->data_recebimento)->format('d/m/Y')}}</td>
+                                <td>{{ $receita->valor}}</td>
+                                <td>
+                                    @if($receita->status == 1)
+                                    <p style="color: green">Pago</p>
+                                    @else
+                                    <p style="color: red">Não Pago</p>
+                                    @endif
+                                </td>
+                                <td>
+                                    {{$receita->categoria->descricao}}
+                                </td>
+                                <td>
+                                    <span class="d-flex">
+                                        <a href="{{route('receitas.edit',$receita->id)}}" class="btn btn-info btn-sm mr-1" data-toggle="modal" data-target="#editModal">
+                                            <i class="fas fa-external-link-alt"></i>
+                                        </a>
+                                    </span>
+                                </td>
+                                <td>
+                                    <form action="{{route('receitas.destroy',$receita->id)}}" method="post" onsubmit="return confirm('Tem certeza que deseja remover {{ addslashes($receita->descricao) }}?')">
+                                        @csrf
+                                        @method('DELETE')
+                                        <button class="btn btn-danger btn-sm">
+                                            <i class="far fa-trash-alt"></i>
+                                        </button>
+                                    </form>
+                                    </span>
+                                </td>
+                            </tr>
+                            @endforeach
+                        </tbody>
+                        <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#myModal">
+                            <i class="fa fa-plus" aria-hidden="true"></i>
+                            Adcionar
+                        </button>
+                    </table>
+                    <div>
+                        <b>Total de Receitas :</b>
+                        <p style="color: green"> {{$total}} </p>
                     </div>
-                    @include('receitas.create')
-                    @include('components.flash-message')
-
-                    <div class="card-body">
-                        <div class="card-body">
-                            <table id="example1" class="table table-bordered table-striped">
-                                <thead>
-                                    <tr>
-                                        <th scope="col">#</th>
-                                        <th scope="col">Descrição da Receita</th>
-                                        <th scope="col">Data do Recebimento</th>
-                                        <th scope="col">Valor</th>
-                                        <th scope="col">Status</th>
-                                        <th scope="col">Categoria</th>
-                                        <th scope="col">Editar</th>
-                                        <th scope="col">Excluir</th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    @foreach($receitas as $receita)
-                                    <tr>
-                                        <th scope="row">{{ $receita->id }}</th>
-                                        <td>{{ $receita->descricao }}</td>
-                                        <td>{{Carbon\Carbon::parse( $receita->data_recebimento)->format('d/m/Y')}}</td>
-                                        <td>{{ $receita->valor}}</td>
-                                        <td>
-                                            @if($receita->status == 1)
-                                            <p style="color: green">Pago</p>
-                                            @else
-                                            <p style="color: red">Não Pago</p>
-                                            @endif
-                                        </td>
-                                        <td>
-                                            {{$receita->categoria->descricao}}
-                                        </td>
-                                        <td>
-                                            <span class="d-flex">
-                                                <a href="{{route('receitas.edit',$receita->id)}}" class="btn btn-info btn-sm mr-1" data-toggle="modal" data-target="#editModal">
-                                                    <i class="fas fa-external-link-alt"></i>
-                                                </a>
-                                            </span>
-                                        </td>
-                                        <td>
-                                            <form action="{{route('receitas.destroy',$receita->id)}}" method="post" onsubmit="return confirm('Tem certeza que deseja remover {{ addslashes($receita->descricao) }}?')">
-                                                @csrf
-                                                @method('DELETE')
-                                                <button class="btn btn-danger btn-sm">
-                                                    <i class="far fa-trash-alt"></i>
-                                                </button>
-                                            </form>
-                                            </span>
-                                        </td>
-                                    </tr>
-                                    @endforeach
-                                </tbody>
-
-                                <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#myModal">
-
-                                    <i class="fa fa-plus" aria-hidden="true"></i>
-                                    Adcionar
-                                </button>
-
-                            </table>
-                            <div><b>Total de Receitas :</b>
-                                <p style="color: green"> {{$total}} </p>
-                            </div>
-        </ul>
+                </div>
+            </div>
+        </div>
     </div>
+    </ul>
+</div>
 </div>
 
 <!-- Modal Edit -->
@@ -106,7 +107,7 @@
                 </button>
             </div>
             <div class="modal-body">
-                 @include('receitas.edit', ['receitas' => $receita])
+                @include('receitas.edit', ['receitas' => $receita])
             </div>
         </div>
     </div>
--- resources/views/relatorios/index.blade.php
+++ resources/views/relatorios/index.blade.php
@@ -4,101 +4,138 @@
     @include('layouts.sidebar')
     <div class="card-body">
         <div class="table-responsive">
-            <div class="card">
-                <div class="card-header">
-                    <h1 class="card-title">Relatório PDF</h1>
-                </div>
-                <div class="row">
-                    <div class="col-md-12">
-                        <br>
+            <div class="card-header">
+                <h1 class="card-title">Relatório PDF</h1>
+            </div>
+            <div class="row">
+                <div class="col-md-12">
+                    <ul class="list-group">
                         <div class="card card-primary">
-                            <form action="{{ route('relatorios.despesas') }}" method="post">
-                                @csrf
-                                <br>
+                            <form action="{{ route('relatorios.despesas')}}" method="post">
                                 <div class="col col-6">
-                                    <label for="filter1">Descrição:</label>
-                                    <input type="text" name="filter1">
+                                    <label for="exampleInputEmail1">Email address</label>
+                                    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+                                    <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
                                 </div>
-                                <br>
-                                <div class="col col-2">
-                                    <label for="categoria_id">Categoria:</label>
-                                    <select name="categoria_id" id="categoria_id" class="form-control">
-                                        <option>Selecione...</option>
-                                        @foreach($categorias as $c)
-                                        <option value="{{ $c->id }}">{{ $c->descricao }}</option>
-                                        @endforeach
-                                    </select>
-                                </div>
-                                <br>
                                 <div class="col col-6">
-                                    <label for="filter2">Data :</label>
-                                    <input type="date" name="filter2">
+                                    <label for="exampleInputPassword1">Password</label>
+                                    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
                                 </div>
-
+                                <div class="form-check">
+                                    <input type="checkbox" class="form-check-input" id="exampleCheck1">
+                                    <label class="form-check-label" for="exampleCheck1">Check me out</label>
+                                </div>
                                 <br>
-                                <button type="submit" class="btn btn-primary">Gerar PDF</button>
+                                <button type="submit" class="btn btn-primary">Submit</button>
                             </form>
                         </div>
-                    </div>
+                    </ul>
                 </div>
             </div>
         </div>
-        <div class="card-body">
-            <table id="example1" class="table table-bordered table-striped">
-                <thead>
-                    <tr>
-                        <th>ID</th>
-                        <th>Coluna 1</th>
-                        <th>Coluna 2</th>
-                        <th>Ações</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td>
-                            <button class="btnSalvar" data-id="">Salvar</button>
-                        </td>
-                    </tr>
-                </tbody>
-            </table>
-        </div>
-
-        <script>
-            $(document).ready(function() {
-                var tabela = $('#example1').DataTable();
-
-                // Configurar o evento de clique para salvar
-                $('#example1 tbody').on('click', 'button.btnSalvar', function() {
-                    var tr = $(this).closest('tr');
-                    var id = tr.find('td:first-child').text();
-                    var coluna1 = tr.find('td:nth-child(2)').text();
-                    var coluna2 = tr.find('td:nth-child(3)').text();
-                    // ... obter outras colunas ...
-
-                    // Enviar dados para o servidor (Laravel) para salvar
-                    $.ajax({
-                        url: '/categorias/update',
-                        type: 'POST',
-                        data: {
-                            id: id,
-                            coluna1: coluna1,
-                            coluna2: coluna2,
-                            // ... enviar outras colunas ...
-                        },
-                        success: function(response) {
-                            // Atualizar DataTable após salvar
-                            tabela.ajax.reload();
-                        },
-                        error: function(error) {
-                            console.error('Erro ao salvar', error);
-                        }
-                    });
-                });
-            });
-        </script>
     </div>
+</div>
+
+<div class="table-responsive">
+    <div class="card">
+        <div class="card-header">
+            <h1 class="card-title">Relatório PDF</h1>
+        </div>
+        <div class="row">
+            <div class="col-md-12">
+                <br>
+                <div class="card card-primary">
+                    <form action="{{ route('relatorios.despesas') }}" method="post">
+                        @csrf
+                        <br>
+                        <div class="col col-6">
+                            <label for="filter1">Descrição:</label>
+                            <input type="text" name="filter1">
+                        </div>
+                        <br>
+                        <div class="col col-2">
+                            <label for="categoria_id">Categoria:</label>
+                            <select name="categoria_id" id="categoria_id" class="form-control">
+                                <option>Selecione...</option>
+                                @foreach($categorias as $c)
+                                <option value="{{ $c->id }}">{{ $c->descricao }}</option>
+                                @endforeach
+                            </select>
+                        </div>
+                        <br>
+                        <div class="col col-6">
+                            <label for="filter2">Data :</label>
+                            <input type="date" name="filter2">
+                        </div>
+                        <div>Entre</div>
+                        <div class="col col-6">
+                            <label for="filter2">Data :</label>
+                            <input type="date" name="filter2">
+                        </div>
+                        <br>
+                        <button type="submit" class="btn btn-primary">Gerar PDF</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="card-body">
+    <table id="example1" class="table table-bordered table-striped">
+        <thead>
+            <tr>
+                <th>ID</th>
+                <th>Coluna 1</th>
+                <th>Coluna 2</th>
+                <th>Ações</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td>
+                    <button class="btnSalvar" data-id="">Salvar</button>
+                </td>
+            </tr>
+        </tbody>
+    </table>
+</div>
+<script>
+    $(document).ready(function() {
+        var tabela = $('#example1').DataTable();
+
+        // Configurar o evento de clique para salvar
+        $('#example1 tbody').on('click', 'button.btnSalvar', function() {
+            var tr = $(this).closest('tr');
+            var id = tr.find('td:first-child').text();
+            var coluna1 = tr.find('td:nth-child(2)').text();
+            var coluna2 = tr.find('td:nth-child(3)').text();
+            // ... obter outras colunas ...
+
+            // Enviar dados para o servidor (Laravel) para salvar
+            $.ajax({
+                url: '/categorias/update',
+                type: 'POST',
+                data: {
+                    id: id,
+                    coluna1: coluna1,
+                    coluna2: coluna2,
+                    // ... enviar outras colunas ...
+                },
+                success: function(response) {
+                    // Atualizar DataTable após salvar
+                    tabela.ajax.reload();
+                },
+                error: function(error) {
+                    console.error('Erro ao salvar', error);
+                }
+            });
+        });
+    });
+</script>
+</ul>
+</div>
 </div>
 @include('layouts.footer')
