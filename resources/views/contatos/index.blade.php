c785aebce00ab64bb271e0acb4c1b151bc0c0907
Agostinhoneto <agostneto6@gmail.com>
2024-08-18 09:53:24 -0300

    alterando index na views

--- resources/views/contatos/index.blade.php
+++ resources/views/contatos/index.blade.php
@@ -1,12 +1,5 @@
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta http-equiv="x-ua-compatible" content="ie=edge">
-    <meta name="referrer" content="always">
-    <title>Admin</title>
-    @vite('resources/css/app.css')
-    @vite('resources/js/app.js')
-</head>
+@include('layouts.topo')
+@extends('layout')
 @extends('layout')
 <div x-data="{ sidebarOpen: false }" class="flex h-screen bg-gray-200 font-roboto">
     @include('layouts.sidebar')
@@ -14,80 +7,51 @@
         <ul class="list-group">
             <div class="table-responsive">
                 <div class="card">
-                    <!-- Content Wrapper. Contains page content -->
-                        <!-- Content Header (Page header) -->
-                        <section class="content-header">
-                            <div class="container-fluid">
-                                <div class="row mb-2">
-                                    <div class="col-sm-6">
-                                        <h1>Contato</h1>
-                                    </div>
-                                    <div class="col-sm-6">
-                                        <ol class="breadcrumb float-sm-right">
-                                            <li class="breadcrumb-item"><a href="#">Home</a></li>
-                                            <li class="breadcrumb-item active">Contact us</li>
-                                        </ol>
+                    <div class="card-header">
+                        <h3 class="card-title">Contatos</h3>
+                    </div>
+                    <section class="content">
+                        <div class="card">
+                            <div class="card-body row">
+                                <div class="col-5 text-center d-flex align-items-center justify-content-center">
+                                    <div class="">
+                                        <h2><strong>Sistemas de Controle de Gastos</strong></h2>
+                                        <p class="lead mb-5">Monte Santo - Bahia<br>
+                                            Telefone: 75 999291-5575
+                                        </p>
                                     </div>
                                 </div>
-                            </div><!-- /.container-fluid -->
-                        </section>
-
-                        <!-- Main content -->
-                        <section class="content">
-
-                            <!-- Default box -->
-                            <div class="card">
-                                <div class="card-body row">
-                                    <div class="col-5 text-center d-flex align-items-center justify-content-center">
-                                        <div class="">
-                                            <h2><strong>Sistemas de Controle de Gastos</strong></h2>
-                                            <p class="lead mb-5">Monte Santo - Bahia<br>
-                                                Telefone: 75 999291-5575
-                                            </p>
-                                        </div>
+                                <div class="col-7">
+                                    <div class="form-group">
+                                        <label for="inputName">Nome</label>
+                                        <input type="text" id="inputName" class="form-control" />
                                     </div>
-                                    <div class="col-7">
-                                        <div class="form-group">
-                                            <label for="inputName">Nome</label>
-                                            <input type="text" id="inputName" class="form-control" />
-                                        </div>
-                                        <div class="form-group">
-                                            <label for="inputEmail">E-Mail</label>
-                                            <input type="email" id="inputEmail" class="form-control" />
-                                        </div>
-                                        <div class="form-group">
-                                            <label for="inputSubject">Assunto</label>
-                                            <input type="text" id="inputSubject" class="form-control" />
-                                        </div>
-                                        <div class="form-group">
-                                            <label for="inputMessage">Mensagem</label>
-                                            <textarea id="inputMessage" class="form-control" rows="4"></textarea>
-                                        </div>
-                                        <div class="form-group">
-                                            <input type="submit" class="btn btn-primary" value="Enviar">
-                                        </div>
+                                    <div class="form-group">
+                                        <label for="inputEmail">E-Mail</label>
+                                        <input type="email" id="inputEmail" class="form-control" />
+                                    </div>
+                                    <div class="form-group">
+                                        <label for="inputSubject">Assunto</label>
+                                        <input type="text" id="inputSubject" class="form-control" />
+                                    </div>
+                                    <div class="form-group">
+                                        <label for="inputMessage">Mensagem</label>
+                                        <textarea id="inputMessage" class="form-control" rows="4"></textarea>
+                                    </div>
+                                    <div class="form-group">
+                                        <input type="submit" class="btn btn-primary" value="Enviar">
                                     </div>
                                 </div>
                             </div>
-                        </section>
-                        <!-- /.content -->
-                    <footer class="main-footer">
-                        <div class="float-right d-none d-sm-block">
-                            <b>Version</b> 3.2.0
                         </div>
-                        <strong>Copyright &copy; 2014-2021 <a href="https://adminlte.io">AdminLTE.io</a>.</strong> All rights reserved.
-                    </footer>
-                    <!-- Control Sidebar -->
-                    <aside class="control-sidebar control-sidebar-dark">
-                        <!-- Control sidebar content goes here -->
-                    </aside>
-                    <!-- /.control-sidebar -->
+                    </section>
                 </div>
-                <!-- ./wrapper -->
             </div>
         </ul>
     </div>
 </div>
+@include('layouts.footer')
+
 <!-- jQuery -->
 <script src="../../plugins/jquery/jquery.min.js"></script>
 <!-- Bootstrap 4 -->
@@ -96,6 +60,3 @@
 <script src="../../dist/js/adminlte.min.js"></script>
 <!-- AdminLTE for demo purposes -->
 <script src="../../dist/js/demo.js"></script>
-</body>
-
-</html>
