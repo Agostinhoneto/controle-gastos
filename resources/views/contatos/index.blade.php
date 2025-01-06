07c4848bb6321b81d3b65bc32941128506d55322
AgostinhoNeto <agostneto6@gmail.com>
2025-01-06 15:27:36 -0300

    alterando front

--- resources/views/contatos/index.blade.php
+++ resources/views/contatos/index.blade.php
@@ -1,64 +1,55 @@
 @include('layouts.topo')
 @extends('layout')
-@extends('layout')
+
 <div x-data="{ sidebarOpen: false }" class="flex h-screen bg-gray-200 font-roboto">
     @include('layouts.sidebar')
-    <div class="card-body">
-        <ul class="list-group">
-            <div class="table-responsive">
-                <div class="card">
-                    <div class="card-header">
-                        <h3 class="card-title">Contatos</h3>
-                    </div>
-                    <section class="content">
-                        <div class="card">
-                            <div class="card-body row">
-                                <div class="col-5 text-center d-flex align-items-center justify-content-center">
-                                    <div class="">
-                                        <h2><strong>Sistemas de Controle de Gastos</strong></h2>
-                                        <p class="lead mb-5">Monte Santo - Bahia<br>
-                                            Telefone: 75 999291-5575
-                                        </p>
-                                    </div>
-                                </div>
-                                <div class="col-6">
-                                    <form action="{{ route('contatos.store') }}" method="POST">
-                                        <div class="form-group">
-                                            <label for="inputName">Nome</label>
-                                            <input type="text" id="inputName" class="form-control" />
-                                        </div>
-                                        <div class="form-group">
-                                            <label for="inputEmail">E-Mail</label>
-                                            <input type="email" id="inputEmail" class="form-control" />
-                                        </div>
-                                        <div class="form-group">
-                                            <label for="inputSubject">Assunto</label>
-                                            <input type="text" id="inputSubject" class="form-control" />
-                                        </div>
-                                        <div class="form-group">
-                                            <label for="inputMessage">Mensagem</label>
-                                            <textarea id="inputMessage" class="form-control" rows="4"></textarea>
-                                        </div>
-                                        <div class="form-group">
-                                            <input type="submit" class="btn btn-primary" value="Enviar">
-                                        </div>
-                                    </form>
-                                </div>
-                            </div>
+
+    <div class="container my-4">
+        <div class="card">
+            <div class="card-header bg-primary text-white">
+                <h3 class="card-title">Contatos</h3>
+            </div>
+            <div class="card-body">
+                <div class="row">
+                    <div class="col-md-6 text-center d-flex align-items-center justify-content-center">
+                        <div>
+                            <h2><strong>Sistema de Controle de Gastos</strong></h2>
+                            <p class="lead mb-4">Monte Santo - Bahia<br>Telefone: (75) 99929-15575</p>
                         </div>
-                    </section>
+                    </div>
+                    <div class="col-md-6">
+                        <form action="{{ route('contatos.store') }}" method="POST">
+                            @csrf
+                            <div class="form-group">
+                                <label for="inputName">Nome</label>
+                                <input type="text" name="name" id="inputName" class="form-control" required />
+                            </div>
+                            <div class="form-group">
+                                <label for="inputEmail">E-mail</label>
+                                <input type="email" name="email" id="inputEmail" class="form-control" required />
+                            </div>
+                            <div class="form-group">
+                                <label for="inputSubject">Assunto</label>
+                                <input type="text" name="subject" id="inputSubject" class="form-control" required />
+                            </div>
+                            <div class="form-group">
+                                <label for="inputMessage">Mensagem</label>
+                                <textarea name="message" id="inputMessage" class="form-control" rows="4" required></textarea>
+                            </div>
+                            <div class="form-group">
+                                <button type="submit" class="btn btn-primary btn-block">Enviar</button>
+                            </div>
+                        </form>
+                    </div>
                 </div>
             </div>
-        </ul>
+        </div>
     </div>
 </div>
+
 @include('layouts.footer')
 
-<!-- jQuery -->
-<script src="../../plugins/jquery/jquery.min.js"></script>
-<!-- Bootstrap 4 -->
-<script src="../../plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
-<!-- AdminLTE App -->
-<script src="../../dist/js/adminlte.min.js"></script>
-<!-- AdminLTE for demo purposes -->
-<script src="../../dist/js/demo.js"></script>
+<!-- Scripts -->
+<script src="{{ asset('plugins/jquery/jquery.min.js') }}"></script>
+<script src="{{ asset('plugins/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+<script src="{{ asset('dist/js/adminlte.min.js') }}"></script>
--- resources/views/relatorios/index.blade.php
+++ resources/views/relatorios/index.blade.php
@@ -1,103 +1,102 @@
 <head>
     <!-- Google Font: Source Sans Pro -->
-    <link href="{{ URL::asset('assets/datatables/dataTables.bootstrap4.min.css') }}" rel="stylesheet">
-    <!-- Scripts DataTables -->
-    <script src="../../plugins/jquery/jquery.min.js"></script>
-    <!-- Bootstrap 4 -->
-    <script src="../../plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
-    <!-- DataTables  & Plugins -->
-    <script src="../../plugins/datatables/jquery.dataTables.min.js"></script>
-    <script src="../../plugins/datatables-bs4/js/dataTables.bootstrap4.min.js"></script>
-    <script src="../../plugins/datatables-responsive/js/dataTables.responsive.min.js"></script>
-    <script src="../../plugins/datatables-responsive/js/responsive.bootstrap4.min.js"></script>
-    <script src="../../plugins/datatables-buttons/js/dataTables.buttons.min.js"></script>
-    <script src="../../plugins/datatables-buttons/js/buttons.bootstrap4.min.js"></script>
-    <script src="../../plugins/jszip/jszip.min.js"></script>
-    <script src="../../plugins/pdfmake/pdfmake.min.js"></script>
-    <script src="../../plugins/pdfmake/vfs_fonts.js"></script>
-    <script src="../../plugins/datatables-buttons/js/buttons.html5.min.js"></script>
-    <script src="../../plugins/datatables-buttons/js/buttons.print.min.js"></script>
-    <script src="../../plugins/datatables-buttons/js/buttons.colVis.min.js"></script>
-    <!-- AdminLTE App -->
-    <script src="../../dist/js/adminlte.min.js"></script>
-    <!-- AdminLTE for demo purposes -->
-    <script src="../../dist/js/demo.js"></script>
-    <!-- Page specific script -->
-    <!-- Google Font: Source Sans Pro -->
+    <link href="{{ asset('assets/datatables/dataTables.bootstrap4.min.css') }}" rel="stylesheet">
     <!-- Font Awesome -->
-    <link rel="stylesheet" href="../../plugins/fontawesome-free/css/all.min.css">
-    <!-- DataTables -->
+    <link rel="stylesheet" href="{{ asset('plugins/fontawesome-free/css/all.min.css') }}">
     <!-- Theme style -->
-    <link rel="stylesheet" href="../../dist/css/adminlte.min.css">
+    <link rel="stylesheet" href="{{ asset('dist/css/adminlte.min.css') }}">
+    <!-- DataTables CSS -->
+    <link rel="stylesheet" href="{{ asset('plugins/datatables-bs4/css/dataTables.bootstrap4.min.css') }}">
 </head>
+
 <div x-data="{ sidebarOpen: false }" class="flex h-screen bg-gray-200 font-roboto">
     @include('layouts.topo')
     @include('layouts.sidebar')
+    
     <div class="card-body">
-        <ul class="list-group">
-            <div class="table-responsive">
-                <div class="card-header">
-                    <h3 class="card-title">Relatórios</h3>
-                </div>
-                <div class="mb-5">
-                    <form method="GET" action="{{ url('/reports') }}">
+        <div class="table-responsive">
+            <div class="card-header">
+                <h3 class="card-title">Relatórios</h3>
+            </div>
+            <div class="mb-5">
+                <form method="GET" action="{{ url('/reports') }}">
+                    <div class="form-group">
                         <label for="created_at">Data Inicial:</label>
-                        <input type="date" name="created_at" id="created_at">
-
+                        <input type="date" name="created_at" id="created_at" class="form-control" required>
+                    </div>
+                    <div class="form-group">
                         <label for="data_pagamento">Data Final:</label>
-                        <input type="date" name="data_pagamento" id="data_pagamento">
-                        <button type="submit" class="btn btn-success">Filtrar</button>
-                    </form>
-                </div>
-                <table id="example1" class="table table-bordered table-striped">
-                    <thead>
-                        <tr>
-                            <th>Id</th>
-                            <th>Descrição</th>
-                            <th>Data</th>
-                            <th>Valor</th>
-                            <th>Status</th>
-                        </tr>
-                    </thead>
-              
-                    <tbody>
-                        @foreach($despesas as $despesa)
+                        <input type="date" name="data_pagamento" id="data_pagamento" class="form-control" required>
+                    </div>
+                    <button type="submit" class="btn btn-success">Filtrar</button>
+                </form>
+            </div>
+            
+            <table id="example1" class="table table-bordered table-striped">
+                <thead>
+                    <tr>
+                        <th>Id</th>
+                        <th>Descrição</th>
+                        <th>Data</th>
+                        <th>Valor</th>
+                        <th>Status</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach($despesas as $despesa)
                         <tr>
                             <td>{{ $despesa->id }}</td>
                             <td>{{ $despesa->descricao }}</td>
-                            <td>{{Carbon\Carbon::parse( $despesa->data_recebimento)->format('d/m/Y')}}</td>
-                            <td>{{ $despesa->valor}}</td>
+                            <td>{{ \Carbon\Carbon::parse($despesa->data_recebimento)->format('d/m/Y') }}</td>
+                            <td>R$ {{ number_format($despesa->valor, 2, ',', '.') }}</td>
                             <td>
                                 @if($despesa->status == 1)
-                                <p style="color: green">Pago</p>
+                                    <p style="color: green">Pago</p>
                                 @else
-                                <p style="color: red">Não Pago</p>
+                                    <p style="color: red">Não Pago</p>
                                 @endif
                             </td>
                         </tr>
-                        @endforeach
+                    @endforeach
+                </tbody>
+            </table>
 
-                </table>
-                <td>{{$total}}</td>
-
+            <div class="mt-3">
+                <strong>Total: </strong> R$ {{ number_format($total, 2, ',', '.') }}
             </div>
-            <!-- /.card-body -->
+
+        </div>
     </div>
 </div>
-</ul>
-</div>
-</div>
+
 @include('layouts.footer')
 
-<<script>
+<!-- Scripts -->
+<script src="{{ asset('plugins/jquery/jquery.min.js') }}"></script>
+<script src="{{ asset('plugins/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+<script src="{{ asset('plugins/datatables/jquery.dataTables.min.js') }}"></script>
+<script src="{{ asset('plugins/datatables-bs4/js/dataTables.bootstrap4.min.js') }}"></script>
+<script src="{{ asset('plugins/datatables-responsive/js/dataTables.responsive.min.js') }}"></script>
+<script src="{{ asset('plugins/datatables-responsive/js/responsive.bootstrap4.min.js') }}"></script>
+<script src="{{ asset('plugins/datatables-buttons/js/dataTables.buttons.min.js') }}"></script>
+<script src="{{ asset('plugins/datatables-buttons/js/buttons.bootstrap4.min.js') }}"></script>
+<script src="{{ asset('plugins/jszip/jszip.min.js') }}"></script>
+<script src="{{ asset('plugins/pdfmake/pdfmake.min.js') }}"></script>
+<script src="{{ asset('plugins/pdfmake/vfs_fonts.js') }}"></script>
+<script src="{{ asset('plugins/datatables-buttons/js/buttons.html5.min.js') }}"></script>
+<script src="{{ asset('plugins/datatables-buttons/js/buttons.print.min.js') }}"></script>
+<script src="{{ asset('plugins/datatables-buttons/js/buttons.colVis.min.js') }}"></script>
+<script src="{{ asset('dist/js/adminlte.min.js') }}"></script><script>
     $(function() {
-        var table = $('#example1').DataTable();
-        table.destroy();
-        $("#example1").DataTable({
-            "responsive": true,
-            "lengthChange": false,
-            "autoWidth": false,
-            "buttons": ["copy", "csv", "excel", "pdf", "print"]
-        }).buttons().container().appendTo('#example1_wrapper .col-md-6:eq(0)');
+        // Verificar se a tabela já foi inicializada
+        if (!$.fn.dataTable.isDataTable('#example1')) {
+            $("#example1").DataTable({
+                "responsive": true,
+                "lengthChange": false,
+                "autoWidth": false,
+                "buttons": ["copy", "csv", "excel", "pdf", "print"]
+            }).buttons().container().appendTo('#example1_wrapper .col-md-6:eq(0)');
+        }
     });
 </script>
+
